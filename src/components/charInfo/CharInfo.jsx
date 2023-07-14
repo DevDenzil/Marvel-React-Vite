115870647bd3031e5743c39803438a91e47a74c3
Denys <denzil92@gmail.com>
2023-07-14 23:35:28 +0200

    ss

---
+++ src/components/charInfo/CharInfo.jsx
@@ -0,0 +1,104 @@
+import { useState, useEffect } from 'react';
+import PropTypes from 'prop-types';
+
+import useMarvelService from '../../services/MarvelService';
+
+import Spinner from '../spinner/Spinner';
+import ErrorMessage from '../errorMessage/ErrorMessage';
+import Skeleton from '../skeleton/Skeleton';
+
+import './charInfo.scss';
+
+const CharInfo = (props) => {
+  const [char, setChar] = useState(null);
+
+  const { getCharacter, clearError, process, setProcess } = useMarvelService();
+  console.log(process)
+  debugger
+
+  useEffect(() => {
+    updateChar();
+  }, [props.charId]);
+
+  const updateChar = () => {
+    clearError();
+    const { charId } = props;
+    if (!charId) {
+      return;
+    }
+
+    getCharacter(charId)
+      .then(onCharLoaded)
+      .then(() => setProcess('confirmed'));
+  };
+
+  const onCharLoaded = (char) => {
+    setChar(char);
+  };
+
+  const setContent = (process, char) => {
+    switch (process) {
+      case 'waiting':
+        return <Skeleton />;
+      case 'loading':
+        return <Spinner />;
+      case 'confirmed':
+        return <View char={char} />;
+      case 'error':
+        return <ErrorMessage />;
+      default:
+        throw new Error('Unexpected process state');
+    }
+  };
+
+  return <div className="char__info">{setContent(process, char)}</div>;
+};
+
+const View = ({ char }) => {
+  const { name, description, thumbnail, homepage, wiki, comics } = char;
+  let imgStyle = { objectFit: 'cover' };
+  if (
+    thumbnail ===
+    'http://i.annihil.us/u/prod/marvel/i/mg/b/40/image_not_available.jpg'
+  ) {
+    imgStyle = { objectFit: 'contain' };
+  }
+
+  return (
+    <>
+      <div className="char__basics">
+        <img src={thumbnail} alt={name} style={imgStyle} />
+        <div>
+          <div className="char__info-name">{name}</div>
+          <div className="char__btns">
+            <a href={homepage} className="button button__main">
+              <div className="inner">homepage</div>
+            </a>
+            <a href={wiki} className="button button__secondary">
+              <div className="inner">Wiki</div>
+            </a>
+          </div>
+        </div>
+      </div>
+      <div className="char__descr">{description}</div>
+      <div className="char__comics">Comics:</div>
+      <ul className="char__comics-list">
+        {comics.length > 0 ? null : 'There is no comics with this character'}
+        {comics.map((item, i) => {
+          if (i > 9) return;
+          return (
+            <li key={i} className="char__comics-item">
+              {item.name}
+            </li>
+          );
+        })}
+      </ul>
+    </>
+  );
+};
+
+CharInfo.propTypes = {
+  charId: PropTypes.number,
+};
+
+export default CharInfo;
